274fa9924531b4281ed607ba2da3be5de0338fb6
Ricardo <ricardo@sistemasdinamica.com.br>
2019-07-31 19:35:09 -0300

    Numero maior entre 3 opcoes

---
+++ js/index.js
@@ -0,0 +1,7 @@
+var valor1 = parseInt(prompt("Digite aqui um valor 1: "));
+var valor2 = parseInt(prompt("Digite aqui um valor 2: "));
+var valor3 = parseInt(prompt("Digite aqui um valor 3: "));
+
+Math.max(valor1, valor2, valor3);
+
+	document.write("O número maior é: " +Math.max(valor1, valor2, valor3));
